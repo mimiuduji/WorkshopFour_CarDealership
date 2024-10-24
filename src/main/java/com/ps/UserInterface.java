ad1846dd11d8263aa554e0c1b87b5c091a8331d2
Your Name <you@example.com>
2024-10-24 14:40:50 -0500

    first commit

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,11 @@
+package com.ps;
+
+public class UserInterface {
+
+    public static void init(){
+        //Loading the dealership and inventory from a file
+    }
+    public static void display (){
+
+    }
+}
